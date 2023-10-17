33ae3d4abf8c22b930a9850be4c122333c73f0cc
sharath <s.sharath2@in.bosch.com>
2023-10-17 14:50:53 +0530

    efre

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,21 @@
+describe('case 1', () => {
+  before(() =>{
+    cy.visit('https://www.demoblaze.com/');
+  })
+  it.skip('click on Phones', () => {
+    cy.get('a[id="itemc"]').contains('Phones').click()
+  })
+
+  it('click on iphone 6 32gb',() =>{
+    cy.get('a[class="hrefch"]').contains('Iphone 6 32gb').click()
+  })
+
+  it('add to cart',() =>{
+    cy.get('a').contains('Add to cart',{timeout:5000}).click()
+  })
+
+  it('click on Home',() =>{
+    cy.get('a').contains('Home').click()
+  })
+})
+

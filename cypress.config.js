36a9a96a0e58338662e57ca4296ed282af35ca56
sharath <s.sharath2@in.bosch.com>
2023-10-20 14:49:22 +0530

    retiress

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     testIsolation:false,
     retries: 3,
+    pageLoadTimeout:10000,
     baseUrl:'https://www.demoblaze.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here

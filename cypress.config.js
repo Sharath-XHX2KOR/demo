0049d37ec75e487df076b14529b287bea34893a3
sharath <s.sharath2@in.bosch.com>
2023-11-08 12:15:24 +0530

    latest push updated dockercompose

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   e2e: {
     testIsolation:false,
     retries: 3,
-    pageLoadTimeout:10000,
-    baseUrl:'https://www.demoblaze.com/',
+    pageLoadTimeout:100000,
+    baseUrl:'https://qa-sp.docs-dev.bosch-fsde.com/saas/dochub/overview/8a80c0d18b426ef5018b42d8728c000c#pricing',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

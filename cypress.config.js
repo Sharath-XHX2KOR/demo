b570aa30a8a17f1eca8d3e297369514af68120c9
sharath <s.sharath2@in.bosch.com>
2023-11-09 14:54:42 +0530

    demo44

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     testIsolation:false,
     retries: 3,
     pageLoadTimeout:100000,
-    baseUrl:'https://qa-sp.docs-dev.bosch-fsde.com/saas/dochub/overview/8a80c0d18b426ef5018b42d8728c000c#pricing',
+    baseUrl:'https://www.demoblaze.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
